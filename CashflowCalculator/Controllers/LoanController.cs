abe5aa81cd9995832723095b99ee1a7eb97c38fa
maxgold45 <maxgold45@gmail.com>
2019-12-04 17:00:45 -0500

    Working on integrating Angular and C#

---
+++ CashflowCalculator/Controllers/LoanController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CashflowCalculator.Models;
+
+
+namespace CashflowCalculator.Controllers
+{
+    public class LoanController : ApiController
+    {
+
+        [HttpGet]
+        public double getInterestPayment(Loan loan)
+        {
+            return loan.balance * loan.rate / 1200;
+        }
+    }
+}
