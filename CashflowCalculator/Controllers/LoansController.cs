f96ed6da621111c2f2c81ff473fa46dfc822a932
maxgold45 <maxgold45@gmail.com>
2019-12-04 15:24:56 -0500

    Started putting loans into MVC.

--- CashflowCalculator/Controllers/LoansController.cs
+++ CashflowCalculator/Controllers/LoansController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 
 namespace CashflowCalculator.Controllers
 {
-    public class CalculatorController : ApiController
+    public class LoansController : ApiController
     {
     }
 }
---
+++ CashflowCalculator/Models/Loan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CashflowCalculator.Models
+{
+    public class Loan
+    {
+        public double balance { get; set; }
+        public int term { get; set; }
+        public double rate { get; set; }
+    }
+}
