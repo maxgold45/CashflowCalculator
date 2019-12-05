1f9fa6a2a043d99b528de29bdcdaf6ed22ac2ba4
maxgold45 <maxgold45@gmail.com>
2019-12-05 09:15:57 -0500

    Moved javascript from index.html into a new file (index.js)

---
+++ CashflowCalculator/index.js
@@ -0,0 +1,26 @@
+﻿var app = angular.module('myApp', []);
+app.controller('calculatorCtrl', function ($scope, $http) {
+    $scope.loan =
+        {
+            balance: "200000",
+            term: "360",
+            rate: ".03"
+        };
+
+    $scope.getInterestPayment = function () {
+
+        $http({
+            method: "GET",
+            url: '../loan/getInterestPayment/' + $scope.loan
+        }).then(function (data) {
+            $scope.loan = [{ balance: data.balance, term: data.term, rate: data.rate }];
+        }, function (data) {
+            $scope.loan = {
+                balance: "",
+                term: "",
+                rate: ""
+            };
+            alert(data.statusText);
+        });
+    };
+});
